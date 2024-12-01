9bbb9385a37ef36d43c90db687d89cfb0a9b0ba2
Aravali-Jeelan-Basha <119558879+Aravali-Jeelan-Basha@users.noreply.github.com>
2024-12-01 23:57:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jeelan <code>src/App.js</code> Basha LPU ka sher.
         </p>
         <a
           className="App-link"
